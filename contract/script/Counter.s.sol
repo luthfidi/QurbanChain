d6ab73a917058213417057c09291e297fdaba5c5
root <root@Bryan.>
2025-06-18 16:58:03 +0700

    set up contract folder

--- contract/script/Counter.s.sol
+++ contract/script/Counter.s.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {QurbanToken} from "../src/QurbanToken.sol";
 
 contract CounterScript is Script {
-    Counter public counter;
+    QurbanToken public counter;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        counter = new QurbanToken();
 
         vm.stopBroadcast();
     }
